ad3162d0b9a2871bb94826760ff450d2d3b635c2
omar-nav <omar.navarro@g.austincc.edu>
2019-09-06 20:39:11 -0500

    see recursiveVowelFinder

--- recursiveVowelFinder.java
+++ recursiveVowelFinder.java
@@ -5,28 +5,32 @@ public class recursiveVowelFinder {
 	static int isVowel(char ch) {
 		ch = Character.toUpperCase(ch);
 		if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-			System.out.println("is a vowel:  " + ch);
+			// why is this return statement able to increment
+			// an implicit variable
+			// that seems elusive to capture
 			return 1;
 		} else {
-			System.out.println("not a vowel:  " + ch);
 			return 0;
 		}
 	}
 
 	// to count total number of vowel from 0 to n 
 	static int countVowels(String str, int n) {
+		int isValue = 0;
 		if (n==1) {
 			System.out.println(str.charAt(n) + " had n == 1");
 			return isVowel(str.charAt(n-1));
 		} else {
 			System.out.println("char at index " + n + ": " + str.charAt(n - 1));
+			isValue += isVowel(str.charAt(n-1));
+			System.out.println("isVowel " + isValue);
 			return countVowels(str, n-1)+isVowel(str.charAt(n-1));
 		}
 	}
 	
 	public static void main(String[] args) {
 		// string object
-		String str = "Jose";
+		String str = "Abcdefghio";
 		
 		// total number of vowels
 		System.out.println(str + " contains " + countVowels(str, str.length()) + " vowels");
